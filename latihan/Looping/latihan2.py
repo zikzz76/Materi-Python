580b925d8faadd1e9b26b3efb7a6c8ad41f3becb
zikzz76 <zrahmadika@gmail.com>
2024-11-19 07:56:02 +0700

    menambah perubahan variabel

---
+++ latihan/Looping/latihan2.py
@@ -0,0 +1,11 @@
+# program cetak teks sebanyak 100 kali dengan looping
+# inisiasi variabel perulangan
+x = 1
+
+# perulangan sebanyak 100 kali
+while x <=100 :
+    # cetak teks
+    print("saya anak Smk Bina Kerja")
+    print("Kelas X-PPL")
+    # tambahkan x dengan 1 setiap perulangan
+    x+=1
